0eae0a95297cfd3a0e5e8a39b81cfaf0115733fe
Sergey Naumov <drmgc@yandex.ru>
2022-07-20 14:54:48 +0300

    feat: requried endpoints in the controller
    * an endpoint that returns the cached data for a given key
    * an endpoint that returns all stored keys in the cache
    * an endpoint that creates/updates the data for a given key
    * an endpoint that removes a given key from the cache
    * an endpoint that removes all keys from the cache

--- src/cache/cache.controller.ts
+++ src/cache/cache.controller.ts
@@ -1,9 +1,63 @@
-import { Controller } from '@nestjs/common'
+import { IsOptional, IsNumber } from 'class-validator'
+import { Type } from 'class-transformer'
+
+import {
+  Controller,
+  Get, Delete, Post,
+  Param, Query,
+} from '@nestjs/common'
 import { CacheService } from './cache.service'
+
+export class FindAllQuery {
+  @IsOptional()
+  @IsNumber()
+  skip?: number
+
+  @IsOptional()
+  @IsNumber()
+  limit?: number
+
+  @IsOptional()
+  @Type(() => Number)
+  includeExpired?: boolean
+}
 
 @Controller('cache')
 export class CacheController {
   constructor(
     private readonly cacheService: CacheService,
   ) {}
+
+  @Get()
+  findAll(
+    @Query() { skip, limit, includeExpired }: FindAllQuery,
+  ): Promise<string[]> {
+    return this.cacheService.findAllKeys({ skip, limit, includeExpired })
+  }
+
+  @Delete()
+  deleteAll(): Promise<void> {
+    return this.cacheService.removeAllKeys()
+  }
+
+  @Get(':key')
+  findByKey(
+    @Param('key') key: string,
+  ): Promise<string> {
+    return this.cacheService.getKey(key)
+  }
+
+  @Post(':key')
+  refreshKey(
+    @Param('key') key: string,
+  ): Promise<string> {
+    return this.cacheService.refreshKey(key)
+  }
+
+  @Delete(':key')
+  deleteByKey(
+    @Param('key') key: string,
+  ): Promise<void> {
+    return this.cacheService.removeKey(key)
+  }
 }
