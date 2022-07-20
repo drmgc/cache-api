6c464d96108d8f7891f9edd0c6b04a5435824e52
Sergey Naumov <drmgc@yandex.ru>
2022-07-20 11:57:48 +0300

    add: @nestjs/config and its usage

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
+
+import { CacheModule } from './cache'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/cache-api'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: async (config: ConfigService) => {
+        return {
+          uri: config.get<string>('MONGODB_URI', 'mongodb://localhost/cache-api'),
+        }
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  const logger = new Logger('main')
+
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  const config = app.get(ConfigService)
+  const port = config.get<number>('PORT', 3000)
+
+  await app.listen(port)
+
+  logger.log(`Listening on port ${port}`)
 }
 bootstrap()
