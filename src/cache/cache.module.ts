bfd661521f74fc85f2d7ecffb79c9c28ebee6d6e
Sergey Naumov <drmgc@yandex.ru>
2022-07-20 12:38:24 +0300

    add: cache depends on ConfigModule

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 
-import { CacheController } from './cache.controller'
 import { schemas } from './schemas'
 import { CacheProvidingService } from './cache-providing.service'
 import { CacheService } from './cache.service'
+import { CacheController } from './cache.controller'
 
 @Module({
   imports: [
+    ConfigModule,
     MongooseModule.forFeature(schemas),
   ],
   controllers: [CacheController],
   providers: [
     CacheProvidingService,
+    CacheService,
+  ],
+  exports: [
     CacheService,
   ],
 })
